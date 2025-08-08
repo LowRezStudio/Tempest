e450ba2035702e79ec8fc258cf168d62ffdcfb10
SupImCinnamon <eckoflesh432@gmail.com>
2025-08-08 15:37:19 +0200

    Add button component

--- Tempest.Launcher/src/lib/components/ui/Button.svelte
+++ Tempest.Launcher/src/lib/components/ui/Button.svelte
@@ -1,1 +1,60 @@
+<script lang="ts">
+	import { goto } from "$app/navigation";
+	import type { Icon } from "@lucide/svelte";
+	import type { HTMLButtonAttributes } from "svelte/elements";
 
+	interface Props extends HTMLButtonAttributes {
+		kind?: "normal" | "icon";
+		icon?: typeof Icon;
+		href?: string;
+		selected?: boolean;
+	}
+
+	let {
+		children,
+		kind = "normal",
+		href,
+		icon,
+		selected,
+		...props
+	}: Props = $props();
+</script>
+
+<button
+	class:icon={kind === "icon"}
+	class:selected={selected ? "selected" : ""}
+	onclick={href ? () => goto(href) : undefined}
+	{...props}
+>
+    <div class="wrapper flex justify-center items-center gap-3">
+        {#if icon}
+            {@const IconComponent = icon}
+            <div class="icon text-white">
+                <IconComponent />
+            </div>
+        {/if}
+        {#if children}
+            <span>
+                {@render children()}
+            </span>
+        {/if}
+    </div>
+</button>
+
+<style>
+	@reference "../../styles/global.css";
+
+	button {
+		@apply bg-[#222329] px-4 py-1.5 rounded-xl flex items-center border-2 border-transparent;
+	}
+	button:hover {
+		@apply cursor-pointer border-transparent border-2 brightness-90;
+	}
+    .selected {
+		@apply ring-2 ring-[var(--color-primary)] bg-[var(--color-primary-highlight)] shadow-lg;
+    }
+    .selected:hover {
+		@apply ring-2 ring-[var(--color-primary)]  shadow-lg;
+    }
+    
+</style>
