a29db57a311741483eaba197cf49a5daadf0050e
Kyiro <54126666+Kyiro@users.noreply.github.com>
2025-08-21 02:10:44 +0200

    add an obscure way to get the windows process id of the game process with wine

--- Tempest.CLI/ProcessUtility.cs
+++ Tempest.CLI/ProcessUtility.cs
@@ -4,29 +4,16 @@ namespace Tempest.CLI;
 
 internal static class ProcessUtility
 {
-    public static bool InjectLibrary(this Process process, string path, bool is64Bit)
-    {
-        var executable = Path.Join(AppContext.BaseDirectory, is64Bit ? "inject64.exe" : "inject32.exe");
-
-        var inject = new Process();
-
-        inject.StartInfo.FileName = executable;
-        inject.StartInfo.Arguments = $"{process.Id} \"{path}\"";
-
-        inject.UseWine().Start();
-        inject.WaitForExit();
-
-        return inject.ExitCode == 0;
-    }
-    
     public static async Task<bool> InjectLibraryAsync(this Process process, string path, bool is64Bit)
     {
         var executable = Path.Join(AppContext.BaseDirectory, is64Bit ? "inject64.exe" : "inject32.exe");
 
+        var pid = await process.GetProcessId();
+
         var inject = new Process();
 
         inject.StartInfo.FileName = executable;
-        inject.StartInfo.Arguments = $"{process.Id} \"{path}\"";
+        inject.StartInfo.Arguments = $"{pid} \"{path}\"";
 
         inject.UseWine().Start();
         await inject.WaitForExitAsync();
