e50631c4040977e38d22bee5ed1513ab08ddfb23
Kyiro <Kyiro@protonmail.com>
2025-12-31 04:47:04 +0100

    add updater

--- Tempest.Launcher/src-tauri/src/lib.rs
+++ Tempest.Launcher/src-tauri/src/lib.rs
@@ -7,6 +7,7 @@ pub fn run() {
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_shell::init())
         .plugin(tauri_plugin_opener::init())
+        .plugin(tauri_plugin_updater::Builder::new().build())
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
