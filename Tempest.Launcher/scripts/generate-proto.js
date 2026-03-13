b37ad66c66098e6291a7bf23cc75073ea824f240
Kyiro <Kyiro@protonmail.com>
2026-03-13 15:48:28 +0100

    refactor: generate-proto.js

--- Tempest.Launcher/scripts/generate-proto.js
+++ Tempest.Launcher/scripts/generate-proto.js
@@ -2,17 +2,17 @@ import { glob, mkdir, rm } from "fs/promises";
 import { join } from "path";
 import $ from "dax";
 
-const outDir = "src/lib/rpc";
+const outDir = join(import.meta.dirname, "../src/lib/rpc");
 
 await rm(outDir, { recursive: true, force: true });
 await mkdir(outDir, { recursive: true });
 
-const protoFiles = [];
-for await (const f of glob("**/*.proto", {
-	cwd: join(import.meta.dirname, "../Tempest.Protocol"),
-})) {
-	protoFiles.push(f);
-}
+const protoFiles = await Array.fromAsync(
+	glob("**/*.proto", {
+		cwd: join(import.meta.dirname, "../../Tempest.Protocol"),
+	}),
+);
+
 const protoFilesStr = protoFiles.join(" ");
 if (!protoFilesStr) {
 	console.error("No proto files found!");
@@ -20,7 +20,7 @@ if (!protoFilesStr) {
 }
 
 console.log("Generating code from .proto files");
-await $`pnpm exec protoc --ts_out ${outDir} --ts_opt server_none --proto_path ../Tempest.Protocol ${$.rawArg(protoFilesStr)}`;
+await $`protoc --ts_out ${outDir} --ts_opt server_none --proto_path ../Tempest.Protocol ${$.rawArg(protoFilesStr)}`;
 
 console.log("Running prettier on the generated files");
-await $`pnpm exec prettier --write ${outDir}/**/*.ts`;
+await $`prettier --write ${outDir}/**/*.ts`;
