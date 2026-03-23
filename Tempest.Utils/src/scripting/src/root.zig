eb6159158933ee4e0968519fa861ab0561fddfc9
Kyiro <Kyiro@protonmail.com>
2026-03-23 23:43:15 +0100

    wip(scripting): consider a memory module

--- Tempest.Utils/src/scripting/src/root.zig
+++ Tempest.Utils/src/scripting/src/root.zig
@@ -2,18 +2,18 @@ const std = @import("std");
 const Lua = @import("luajit").Lua;
 const windows = std.os.windows;
 
+pub const unreal = @import("unreal/root.zig");
+
 extern "kernel32" fn AllocConsole() callconv(.winapi) windows.BOOL;
 
-fn main() !void {
-    _ = AllocConsole();
-
-    var gpa = std.heap.GeneralPurposeAllocator(.{}){};
-    const lua = try Lua.init(gpa.allocator());
+fn initScript(allocator: std.mem.Allocator) !void {
+    const lua = try Lua.init(allocator);
     defer lua.deinit();
 
     lua.openLibs();
 
     try @import("ext/console/root.zig").init(lua);
+    try @import("ext/memory/root.zig").init(lua);
 
     lua.doString(
         \\print("[Lua] Hello World!")
@@ -27,6 +27,14 @@ fn main() !void {
             return;
         },
     };
+}
+
+fn main() !void {
+    _ = AllocConsole();
+
+    const allocator = std.heap.page_allocator;
+
+    try initScript(allocator);
 }
 
 pub export fn DllMain(
